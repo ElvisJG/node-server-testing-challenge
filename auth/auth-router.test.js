33f5424d44157cd47855a6be94ed2c7b8d5620c5
Elvis Gonzalez <ElvisJGonzalez@gmail.com>
2019-09-12 22:55:39 -0400

    Router complete

--- auth/auth-router.test.js
+++ auth/auth-router.test.js
@@ -1,5 +1,6 @@
 const request = require('supertest');
 const server = require('../api/server');
+const db = require('../data/dbConfig.js');
 
 describe('Auth Server', () => {
   test('tests are running with DB_ENV set to testing', () => {
@@ -11,7 +12,13 @@ describe('Auth Server', () => {
     password: 'pass123',
     department: 'sales'
   };
+
+  let token;
+
   describe('POST /register', () => {
+    beforeEach(async () => {
+      await db('users').truncate();
+    });
     test('should returns status 201', () => {
       return request(server)
         .post('/api/register')
@@ -30,6 +37,19 @@ describe('Auth Server', () => {
         .then(res => {
           expect(res.body).toHaveProperty('message');
           expect(res.body).toHaveProperty('token');
+          token = res.body.token;
+        });
+    });
+  });
+
+  describe('GET /users', () => {
+    test('should returns status 200', () => {
+      return request(server)
+        .get('/api/users')
+        .set('Authorization', token)
+        .send(user)
+        .then(res => {
+          expect(res.status).toBe(200);
         });
     });
   });
